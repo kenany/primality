ecd4e3cea7f6897dc7396d29514c8c1071190e05
Kenan Yildirim <kenan@kenany.me>
2012-11-08 17:09:56 -0500

    Use requirejs in tests

---
+++ test/spec/suite.js
@@ -0,0 +1,28 @@
+require(['primality'], function (primality) {
+  describe('Suite', function() {
+    describe('primality()', function() {
+      it('should expose version number', function () {
+        primality.version.should.match(/[0-9]+\.[0-9]+\.[0-9]+/);
+      });
+      it('should expose public constructors', function () {
+        primality.should.be.a('function');
+      });
+      it('should return true when value is prime', function() {
+        primality(7).should.equal(true);
+        primality(new Number(11)).should.equal(true);
+      });
+      it('should return false when value is not prime', function() {
+        primality(6).should.equal(false);
+        primality(new Number(8)).should.equal(false);
+      });
+      it('should return false for 1', function() {
+        primality(1).should.equal(false);
+        primality(new Number(1)).should.equal(false);
+      });
+      it('should turn strings into numbers', function() {
+        primality('13').should.equal(true);
+        primality(new String('24')).should.equal(false);
+      });
+    });
+  });
+});
