d4f4b1855c02b46bbda7b5c001d53f7ce68c9682
Kenan Yildirim <kenan@kenany.me>
2013-01-09 16:06:22 -0500

    Organize tests a little better

--- test/spec/suite.js
+++ test/spec/suite.js
@@ -1,12 +1,12 @@
 require(['primality'], function (primality) {
-  describe('Suite', function() {
-    describe('primality()', function() {
-      it('should expose version number', function () {
-        primality.VERSION.should.match(/[0-9]+\.[0-9]+\.[0-9]+/);
-      });
-      it('should expose public constructor', function () {
-        primality.should.be.a('function');
-      });
+  describe('primality', function() {
+    it('should expose version number', function () {
+      primality.VERSION.should.match(/[0-9]+\.[0-9]+\.[0-9]+/);
+    });
+    it('should expose public constructor', function () {
+      primality.should.be.a('function');
+    });
+    describe('numbers', function() {
       it('should return true when value is prime', function() {
         primality(7).should.equal(true);
         primality(new Number(11)).should.equal(true);
@@ -15,14 +15,20 @@ require(['primality'], function (primality) {
         primality(6).should.equal(false);
         primality(new Number(8)).should.equal(false);
       });
-      it('should return false for 1', function() {
-        primality(1).should.equal(false);
-        primality(new Number(1)).should.equal(false);
+      describe('special cases', function() {
+        it('should return false for 1', function() {
+          primality(1).should.equal(false);
+          primality(new Number(1)).should.equal(false);
+        });
       });
+    });
+    describe('strings', function() {
       it('should turn strings into numbers', function() {
         primality('13').should.equal(true);
         primality(new String('24')).should.equal(false);
       });
+    });
+    describe('arrays', function() {
       it('should return true if an array of prime numbers is passed', function() {
         primality([17, 19, 23]).should.equal(true);
         primality([29, 30, 31]).should.equal(false);
