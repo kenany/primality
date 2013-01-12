5c8f624f9cb3952dce387026332601a74190ef5a
Kenan Yildirim <kenan@kenany.me>
2013-01-11 21:57:57 -0500

    Organize benchmarks

--- perf/index.js
+++ perf/index.js
@@ -5,33 +5,35 @@ var Benchmark = require('benchmark'),
     suite     = new Benchmark.Suite;
 
 /* This first test is always the fastest because less obstacles are in the way */
-suite.add('primality(integer)', function() {
+suite.add('integer', function() {
   primality(2);
   primality(17);
   primality(839);
   primality(3733);
   primality(999983);
-})
+});
+
+/* Should theoretically be as fast as the first test */
+suite.add('integerArray', function() {
+  primality([2, 17, 839, 3733, 999983]);
+});
 
 /* Here, the strings have to be coerced */
-suite.add('primality(string)', function() {
+suite.add('string', function() {
   primality('2');
   primality('17');
   primality('839');
   primality('3733');
   primality('999983');
-})
+});
 
-/* Should theoretically be as fast as the first test */
-suite.add('primality(integerArray)', function() {
-  primality([2, 17, 839, 3733, 999983]);
-})
-
-/* Hopefully as fast as the second test */
-suite.add('primality(stringArray)', function() {
+/* Hopefully as fast as the third test */
+suite.add('stringArray', function() {
   primality(['2', '17', '839', '3733', '999983']);
-})
-.on('cycle', function(event) {
+});
+
+/* Other stuff */
+suite.on('cycle', function(event) {
   console.log(String(event.target));
 })
 .on('complete', function() {
