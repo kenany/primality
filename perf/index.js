a53e7bf8454fac62845d970da703a358f1d8687f
Kenan Yildirim <kenan@kenany.me>
2013-01-11 21:33:04 -0500

    Documented benchmarks

--- perf/index.js
+++ perf/index.js
@@ -2,7 +2,7 @@ var Benchmark = require('benchmark'),
     primality = require('../primality.js'),
     suite     = new Benchmark.Suite;
 
-// add tests
+/* This first test is always the fastest because less obstacles are in the way */
 suite.add('primality(integer)', function() {
   primality(2);
   primality(17);
@@ -10,6 +10,8 @@ suite.add('primality(integer)', function() {
   primality(3733);
   primality(999983);
 })
+
+/* Here, the strings have to be coerced */
 suite.add('primality(string)', function() {
   primality('2');
   primality('17');
@@ -17,9 +19,13 @@ suite.add('primality(string)', function() {
   primality('3733');
   primality('999983');
 })
+
+/* Should theoretically be as fast as the first test */
 suite.add('primality(integerArray)', function() {
   primality([2, 17, 839, 3733, 999983]);
 })
+
+/* Hopefully as fast as the second test */
 suite.add('primality(stringArray)', function() {
   primality(['2', '17', '839', '3733', '999983']);
 })
@@ -27,6 +33,6 @@ suite.add('primality(stringArray)', function() {
   console.log(String(event.target));
 })
 .on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  console.log('\nFastest is ' + this.filter('fastest').pluck('name'));
 })
 .run({ 'async': true });
