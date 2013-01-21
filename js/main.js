d1c0e5dbc75349c1c2b84a7fbd1ad6c3799e22bd
Kenan Yildirim <kenan@kenany.me>
2013-01-21 10:47:29 -0500

    Remove "jquery." from Magellan's name

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ requirejs.config({
   baseUrl: '../vendor/requirejs',
   paths: {
     'jquery': '../jquery/jquery.min',
-    'jquery.foundationMagellan': '../../js/plugins'
+    'foundationMagellan': '../../js/plugins'
   },
   shim: {
-    'jquery.foundationMagellan': {
+    'foundationMagellan': {
       deps: ['jquery'],
       exports: 'jQuery.fn.foundationMagellan'
     }
   }
 });
-require(['jquery.foundationMagellan'], function() {
+require(['foundationMagellan'], function() {
   $(document).foundationMagellan({threshold: 75});
 });
