b56bbecb2a2cac8e1f90a5a38f0a1ceebc5c16de
Kenan Yildirim <kenan@kenany.me>
2013-03-16 23:57:38 -0400

    Add benchmarks for numbers.js

--- perf/index.js
+++ perf/index.js
@@ -2,41 +2,62 @@
 
 var Benchmark = require('benchmark'),
     primality = require('../primality.js'),
+    numbers   = require('numbers'),
     suite     = new Benchmark.Suite;
 
-/* This first test is (usually) the fastest because less obstacles are in the way */
-suite.add('integer', function() {
-  primality(2);
-  primality(17);
-  primality(839);
-  primality(3733);
-  primality(999983);
-});
+suite
+  .add('primality:integer', function() {
+    primality(2);
+    primality(17);
+    primality(839);
+    primality(3733);
+    primality(999983);
+  })
+  .add('primality:integerArray', function() {
+    primality([2, 17, 839, 3733, 999983]);
+  })
+  .add('primality:string', function() {
+    primality('2');
+    primality('17');
+    primality('839');
+    primality('3733');
+    primality('999983');
+  })
+  .add('primality:stringArray', function() {
+    primality(['2', '17', '839', '3733', '999983']);
+  })
 
-/* Theoretically as fast as the first test */
-suite.add('integerArray', function() {
-  primality([2, 17, 839, 3733, 999983]);
-});
+  .add('numbers:integer', function() {
+    numbers.prime.simple(2);
+    numbers.prime.simple(17);
+    numbers.prime.simple(839);
+    numbers.prime.simple(3733);
+    numbers.prime.simple(999983);
+  })
+  .add('numbers:integerArray', function() {
+    var refArray = [2, 17, 839, 3733, 999983];
+    for (var i = 0, len = refArray.length; i < len; i++) {
+      numbers.prime.simple(refArray[i]);
+    }
+  })
+  .add('numbers:string', function() {
+    numbers.prime.simple('2');
+    numbers.prime.simple('17');
+    numbers.prime.simple('839');
+    numbers.prime.simple('3733');
+    numbers.prime.simple('999983');
+  })
+  .add('numbers:stringArray', function() {
+    var refArray = ['2', '17', '839', '3733', '999983'];
+    for (var i = 0, len = refArray.length; i < len; i++) {
+      numbers.prime.simple(refArray[i]);
+    }
+  })
 
-/* Here, the strings have to be coerced */
-suite.add('string', function() {
-  primality('2');
-  primality('17');
-  primality('839');
-  primality('3733');
-  primality('999983');
-});
-
-/* Hopefully as fast as the third test */
-suite.add('stringArray', function() {
-  primality(['2', '17', '839', '3733', '999983']);
-});
-
-/* Other stuff */
-suite.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('\nFastest is ' + this.filter('fastest').pluck('name'));
-})
-.run({ 'async': true });
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('\nFastest is ' + this.filter('fastest').pluck('name'));
+  })
+  .run({'async': true});
