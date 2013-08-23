0a3c9ed0878c8d9865bbd21dc0f9376deca5b156
Kenan Yildirim <kenan@kenany.me>
2013-08-22 22:37:33 -0400

    Use else-ifs instead of ifs

--- dist/primality.js
+++ dist/primality.js
@@ -115,14 +115,7 @@
       if (n === 0) return 0; else if (n % 1 || n * n < 2) return 1; else if (n % 2 === 0) return 2; else if (n % 3 === 0) return 3; else if (n % 5 === 0) return 5;
       var m = Math.sqrt(n);
       for (var i = 7; i <= m; i += 30) {
-        if (n % i === 0) return i;
-        if (n % (i + 4) === 0) return i + 4;
-        if (n % (i + 6) === 0) return i + 6;
-        if (n % (i + 10) === 0) return i + 10;
-        if (n % (i + 12) === 0) return i + 12;
-        if (n % (i + 16) === 0) return i + 16;
-        if (n % (i + 22) === 0) return i + 22;
-        if (n % (i + 24) === 0) return i + 24;
+        if (n % i === 0) return i; else if (n % (i + 4) === 0) return i + 4; else if (n % (i + 6) === 0) return i + 6; else if (n % (i + 10) === 0) return i + 10; else if (n % (i + 12) === 0) return i + 12; else if (n % (i + 16) === 0) return i + 16; else if (n % (i + 22) === 0) return i + 22; else if (n % (i + 24) === 0) return i + 24;
       }
       return n;
     }
--- primality.js
+++ primality.js
@@ -72,14 +72,14 @@ function leastFactor(n) {
 
   var m = Math.sqrt(n);
   for (var i = 7; i <= m; i += 30) {
-    if (n % i === 0)        return i;
-    if (n % (i + 4) === 0)  return i + 4;
-    if (n % (i + 6) === 0)  return i + 6;
-    if (n % (i + 10) === 0) return i + 10;
-    if (n % (i + 12) === 0) return i + 12;
-    if (n % (i + 16) === 0) return i + 16;
-    if (n % (i + 22) === 0) return i + 22;
-    if (n % (i + 24) === 0) return i + 24;
+    if (n % i === 0) return i;
+    else if (n % (i + 4) === 0) return i + 4;
+    else if (n % (i + 6) === 0) return i + 6;
+    else if (n % (i + 10) === 0) return i + 10;
+    else if (n % (i + 12) === 0) return i + 12;
+    else if (n % (i + 16) === 0) return i + 16;
+    else if (n % (i + 22) === 0) return i + 22;
+    else if (n % (i + 24) === 0) return i + 24;
   }
   return n;
 }
